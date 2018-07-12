84632ab9ab05165f2f7c7a663a323429b8b92eec
Max Justicz <max@justi.cz>
2018-07-12 13:44:48 -0700

    update to 16 bit instructions

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,35 @@ use std::fs::File;
 enum Register {
     Reg0,
     Reg1,
+    Reg2,
+    Reg3,
     RegFP,
-    RegAcc,
-    RegSP
+    RegSP,
+    RegAC,
+    RegPC
 }
 
 #[derive(Debug)]
 struct Immediate {
-    value: u8
+    value: u16
+}
+
+#[derive(Debug)]
+struct JMPFlag {
+    relative: bool
 }
 
 impl Register {
-    fn from_code(code: u8) -> Register {
+    fn from_code(code: u16) -> Register {
         match code {
             0x0 => Register::Reg0,
             0x1 => Register::Reg1,
-            0x2 => Register::RegFP,
-            0x3 => Register::RegAcc,
+            0x2 => Register::Reg2,
+            0x3 => Register::Reg3,
             0x4 => Register::RegSP,
+            0x5 => Register::RegFP,
+            0x6 => Register::RegAC,
+            0x7 => Register::RegPC,
             _ => panic!("Invalid register")
         }
     }
@@ -31,46 +42,67 @@ impl Register {
 
 #[derive(Debug)]
 enum Instruction {
-    Add(Register, Register),
-    AddI(Immediate),
-    Mul(Register, Register),
-    MulI(Immediate),
+    ADD(Register, Register, Register),
+    ADDI(Register, Immediate),
+
+    MUL(Register, Register, Register),
+    MULI(Register, Immediate),
+
+    AND(Register, Register, Register),
+    OR(Register, Register, Register),
+    XOR(Register, Register, Register),
+    MOV(Register, Register),
+
+    NEG(Register, Register),
+
     LD(Register, Register),
     ST(Register, Register),
-    ASP(Register),
-    RSP(Register),
-    BNEZ(Register, Register),
-    JMP(Register),
-    SH48(Immediate),
-    AND(Register, Register),
-    OR(Register, Register),
-    XOR(Register, Register),
-    NEG(Register)
+
+    BZ(Register, Register, JMPFlag),
+    BNZ(Register, Register, JMPFlag),
+
+    HLT()
 }
 
 impl Instruction {
-    fn from_byte(byte: u8) -> Instruction {
-        let opcode: u8 = byte >> 4;
-        let reg_x = Register::from_code((byte >> 2) & 3);
-        let reg_y = Register::from_code(byte & 3);
-        let imm = Immediate { value: byte & 15 };
+    fn from_word(word: u16) -> Instruction {
+        let opcode = word >> 12;
+
+        // opcd sc0 sc1 dst xxx
+        // 1111 111 111 111 111
+        let reg_x = Register::from_code((word >> 9) & 0x7);
+        let reg_y = Register::from_code((word >> 6) & 0x7);
+        let reg_z = Register::from_code((word >> 3) & 0x7);
+
+        // opcd sc0 immediate
+        // 1111 111 111111111
+        let imm = Immediate { value: word & 0x1FF };
+
+        // opcd sc0 jmp r xxxxx
+        // 1111 111 111 1 11111
+        let jmp_flag = JMPFlag { relative: ((word >> 5) & 1) == 1};
 
         match opcode {
-            0x0 => Instruction::Add(reg_x, reg_y),
-            0x1 => Instruction::AddI(imm),
-            0x2 => Instruction::Mul(reg_x, reg_y),
-            0x3 => Instruction::MulI(imm),
-            0x4 => Instruction::LD(reg_x, reg_y),
-            0x5 => Instruction::ST(reg_x, reg_y),
-            0x6 => Instruction::ASP(reg_x),
-            0x7 => Instruction::RSP(reg_x),
-            0x8 => Instruction::BNEZ(reg_x, reg_y),
-            0x9 => Instruction::JMP(reg_x),
-            0xa => Instruction::SH48(imm),
-            0xb => Instruction::AND(reg_x, reg_y),
-            0xc => Instruction::OR(reg_x, reg_y),
-            0xd => Instruction::XOR(reg_x, reg_y),
-            0xe => Instruction::NEG(reg_x),
+            0x0 => Instruction::ADD(reg_x, reg_y, reg_z),
+            0x1 => Instruction::ADDI(reg_x, imm),
+
+            0x2 => Instruction::MUL(reg_x, reg_y, reg_z),
+            0x3 => Instruction::MULI(reg_x, imm),
+            
+            0x4 => Instruction::AND(reg_x, reg_y, reg_z),
+            0x5 => Instruction::OR(reg_x, reg_y, reg_z),
+            0x6 => Instruction::XOR(reg_x, reg_y, reg_z),
+            0x7 => Instruction::MOV(reg_x, reg_y),
+            
+            0x8 => Instruction::NEG(reg_x, reg_y),
+
+            0x9 => Instruction::LD(reg_x, reg_y),
+            0xa => Instruction::ST(reg_x, reg_y),
+            
+            0xb => Instruction::BZ(reg_x, reg_y, jmp_flag),
+            0xc => Instruction::BNZ(reg_x, reg_y, jmp_flag),
+
+            0xf => Instruction::HLT(),
             _ => panic!("Invalid opcode")
         }
     }
@@ -78,39 +110,46 @@ impl Instruction {
 
 #[derive(Debug)]
 struct Processor {
-    pc: u16,
-    reg0: u16,
-    reg1: u16,
-    fp: u16,
+    r0: u16,
+    r1: u16,
+    r2: u16,
+    r3: u16,
     sp: u16,
-    acc: u16
+    fp: u16,
+    ac: u16,
+    pc: u16
 }
 
 impl Processor {
-    fn execute_next(&mut self, rom: &[u8; 65536], ram: &mut [u8; 65536]) {
-        let instruction = Instruction::from_byte(rom[self.pc as usize]);
-
-        self.pc = self.pc.wrapping_add(1);
+    fn execute_next(&mut self, rom: &[u16; 65536], ram: &mut [u16; 65536]) {
+        let instruction = Instruction::from_word(rom[self.pc as usize]);
 
         match instruction {
-            Instruction::Add(ref reg_x, ref reg_y) => {
-                let new_value = self.read_register(&Register::RegAcc).wrapping_add(self.read_register(reg_x));
-                self.write_register(reg_y, new_value);
+            Instruction::ADD(ref reg_x, ref reg_y, ref reg_z) => {
+                let new_value = self.read_register(reg_x).wrapping_add(self.read_register(reg_y));
+                self.write_register(reg_z, new_value);
             },
 
-            Instruction::AddI(ref imm) => {
-                let new_value = self.read_register(&Register::RegAcc).wrapping_add(imm.value as u16);
-                self.write_register(&Register::RegAcc, new_value);
+            Instruction::ADDI(ref reg_x, ref imm) => {
+                let reg_x_value = self.read_register(reg_x);
+                let new_value = reg_x_value.wrapping_add(imm.value as u16);
+                self.write_register(&Register::RegAC, new_value);
             },
 
-            Instruction::Mul(ref reg_x, ref reg_y) => {
-                let new_value = self.read_register(&Register::RegAcc).wrapping_mul(self.read_register(reg_x));
-                self.write_register(reg_y, new_value);
+            Instruction::MUL(ref reg_x, ref reg_y, ref reg_z) => {
+                let new_value = self.read_register(reg_x).wrapping_mul(self.read_register(reg_y));
+                self.write_register(reg_z, new_value);
             },
 
-            Instruction::MulI(ref imm) => {
-                let new_value = self.read_register(&Register::RegAcc).wrapping_mul(imm.value as u16);
-                self.write_register(&Register::RegAcc, new_value);
+            Instruction::MULI(ref reg_x, ref imm) => {
+                let reg_x_value = self.read_register(reg_x);
+                let new_value = reg_x_value.wrapping_mul(imm.value as u16);
+                self.write_register(&Register::RegAC, new_value);
+            },
+
+            Instruction::MOV(ref reg_x, ref reg_y) => {
+                let reg_x_value = self.read_register(reg_x);
+                self.write_register(reg_y, reg_x_value);
             },
 
             Instruction::LD(ref reg_x, ref reg_y) => {
@@ -121,118 +160,125 @@ impl Processor {
 
             Instruction::ST(ref reg_x, ref reg_y) => {
                 let store_address = self.read_register(reg_y) as usize;
-                ram[store_address] = self.read_register(reg_x) as u8;
+                ram[store_address] = self.read_register(reg_x) as u16;
             },
 
-            Instruction::ASP(ref reg_x) => {
-                let old_value = self.read_register(&Register::RegSP);
-                let new_value = old_value.wrapping_add(self.read_register(reg_x));
-                self.write_register(&Register::RegSP, new_value);
-            },
-
-            Instruction::RSP(ref reg_x) => {
-                let rsp_value = self.read_register(&Register::RegSP);
-                self.write_register(reg_x, rsp_value);
-            },
-
-            Instruction::BNEZ(ref reg_x, ref reg_y) => {
+            Instruction::BZ(ref reg_x, ref reg_y, ref jmp_flag) => {
                 let reg_x_value = self.read_register(reg_x);
                 let reg_y_value = self.read_register(reg_y);
-                if reg_x_value != 0 {
-                    self.pc = reg_y_value;
+
+                if self.read_register(reg_x) == 0 {
+                    if jmp_flag.relative {
+                        self.pc = self.pc.wrapping_add(reg_y_value);
+                    } else {
+                        self.pc = reg_y_value;
+                    }
                 }
             },
-            Instruction::JMP(ref reg_x) => {
+            Instruction::BNZ(ref reg_x, ref reg_y, ref jmp_flag) => {
                 let reg_x_value = self.read_register(reg_x);
-                self.pc = reg_x_value;
-            },
-            Instruction::SH48(ref imm) => {
-                let imm = imm.value as u16;
-                let old_value = self.read_register(&Register::RegAcc);
-                let mut new_value = 0;
-                if imm == 12 {
-                    new_value = old_value << 4;
-                } else if imm == 14 {
-                    new_value = old_value << 8;
-                } else if imm == 4 {
-                    new_value = old_value >> 4;
-                } else if imm == 6 {
-                    new_value = old_value >> 8;
+                let reg_y_value = self.read_register(reg_y);
+
+                if self.read_register(reg_x) != 0 {
+                    if jmp_flag.relative {
+                        self.pc = self.pc.wrapping_add(reg_y_value);
+                    } else {
+                        self.pc = reg_y_value;
+                    }
                 }
-                self.write_register(&Register::RegAcc, new_value);
             },
-            Instruction::AND(ref reg_x, ref reg_y) => {
-                let reg_x_value = self.read_register(reg_x);
-                let acc_value = self.read_register(&Register::RegAcc);
-                self.write_register(reg_y, reg_x_value & acc_value);
+            Instruction::AND(ref reg_x, ref reg_y, ref reg_z) => {
+                let new_value = self.read_register(reg_x) & self.read_register(reg_y);
+                self.write_register(reg_z, new_value);
             },
-            Instruction::OR(ref reg_x, ref reg_y) => {
-                let reg_x_value = self.read_register(reg_x);
-                let acc_value = self.read_register(&Register::RegAcc);
-                self.write_register(reg_y, reg_x_value | acc_value);
+            Instruction::OR(ref reg_x, ref reg_y, ref reg_z) => {
+                let new_value = self.read_register(reg_x) | self.read_register(reg_y);
+                self.write_register(reg_z, new_value);
             },
-            Instruction::XOR(ref reg_x, ref reg_y) => {
-                let reg_x_value = self.read_register(reg_x);
-                let acc_value = self.read_register(&Register::RegAcc);
-                self.write_register(reg_y, reg_x_value ^ acc_value);
+            Instruction::XOR(ref reg_x, ref reg_y, ref reg_z) => {
+                let new_value = self.read_register(reg_x) ^ self.read_register(reg_y);
+                self.write_register(reg_z, new_value);
             },
-            Instruction::NEG(ref reg_x) => {
-                let acc_value = self.read_register(&Register::RegAcc);
-                self.write_register(reg_x, (acc_value ^ 0xFFFF).wrapping_add(1));
+            Instruction::NEG(ref reg_x, ref reg_y) => {
+                let new_value = self.read_register(reg_x).wrapping_mul(0xFFFF);
+                self.write_register(reg_y, new_value);
+            },
+            Instruction::HLT() => {
+                std::process::exit(0);
             }
-        }
+        };
+
+        self.pc = self.pc.wrapping_add(1);
     }
 
     fn write_register(&mut self, register: &Register, val: u16) {
         match *register {
-            Register::Reg0 => self.reg0 = val,
-            Register::Reg1 => self.reg1 = val,
+            Register::Reg0 => self.r0 = val,
+            Register::Reg1 => self.r1 = val,
+            Register::Reg2 => self.r2 = val,
+            Register::Reg3 => self.r3 = val,
             Register::RegFP => self.fp = val,
             Register::RegSP => self.sp = val,
-            Register::RegAcc => self.acc = val
+            Register::RegAC => self.ac = val,
+            Register::RegPC => self.pc = val
         }
     }
 
     fn read_register(&self, register: &Register) -> u16 {
         match *register {
-            Register::Reg0 => self.reg0,
-            Register::Reg1 => self.reg1,
+            Register::Reg0 => self.r0,
+            Register::Reg1 => self.r1,
+            Register::Reg2 => self.r2,
+            Register::Reg3 => self.r3,
             Register::RegFP => self.fp,
             Register::RegSP => self.sp,
-            Register::RegAcc => self.acc
+            Register::RegAC => self.ac,
+            Register::RegPC => self.pc
         }
     }
 }
 
 fn main() {
-    let mut rom: [u8; 65536] = [0; 65536];
-    let mut ram: [u8; 65536] = [0; 65536];
+    let mut rom: [u16; 65536] = [0xFF; 65536];
+    let mut ram: [u16; 65536] = [0; 65536];
 
-    let program = File::open("/tmp/henlo.bin").unwrap();
+    let mut program = File::open("/tmp/henlo.bin").unwrap();
 
-    for (i, byte) in program.bytes().enumerate() {
-        if i > rom.len() - 1 {
-            panic!("Program too big for ROM")
+    let mut bbuf = [0; 2];
+    let mut i = 0;
+    loop {
+        match program.read_exact(&mut bbuf) {
+            Ok(v) => v,
+            Err(e) => {
+                match e.kind() {
+                    std::io::ErrorKind::UnexpectedEof => {
+                        break;
+                    },
+                    _ => panic!("Error reading program: {:?}", e)
+                }
+            }
         }
-        let byte = byte.unwrap();
-        rom[i] = byte;
-        println!("{}: {:?}", i, Instruction::from_byte(byte));
+        rom[i] = (bbuf[0] as u16) * 256 + (bbuf[1] as u16);
+        i += 1;
     }
 
     let mut processor = Processor {
-        pc: 0,
-        acc: 0,
-        reg0: 0,
-        reg1: 0,
+        r0: 0,
+        r1: 0,
+        r2: 0,
+        r3: 0,
+        sp: 0,
         fp: 0,
-        sp: 0
+        ac: 0,
+        pc: 0
     };
 
     let start = Instant::now();
-    for _ in 1..200 {
+    for _ in 1..100 {
+        println!("{:?} {:?}", processor, Instruction::from_word(rom[processor.pc as usize]));
         processor.execute_next(&rom, &mut ram);
-        println!("{:?} {:?}", processor, Instruction::from_byte(rom[processor.pc as usize]));
     }
+
     let elapsed = start.elapsed();
     println!("{:?} ms", (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64);
 }
